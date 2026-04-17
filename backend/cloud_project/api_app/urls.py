0ab34db8b71d6578fc7212878e4548f8d58ede96
wrk28 <andrei.wrk.28@gmail.com>
2026-04-17 18:02:22 -0400

    Add download

--- backend/cloud_project/api_app/urls.py
+++ backend/cloud_project/api_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from api_app.views import UserView, FileView, FileUploadView
+from api_app.views import UserView, FileView, FileUploadView, FileDownloadView
 
 
 urlpatterns = [
     path('users/', UserView.as_view()),
     path('files/', FileView.as_view()),
     path('files/upload/', FileUploadView.as_view()),
-    path('files/download/', FileUploadView.as_view())
+    path('files/download/', FileDownloadView.as_view())
 ]
