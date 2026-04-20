5d310200ff7784f4f21601c77b23656b8a279665
wrk28 <andrei.wrk.28@gmail.com>
2026-04-19 23:02:41 -0400

    add validation

--- backend/cloud_storage/api/auth/serializers.py
+++ backend/cloud_storage/api/auth/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from re import match
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -17,3 +18,29 @@ class RegisterSerializer(serializers.ModelSerializer):
             last_name=validate_data['last_name'],
         )
         return user
+    
+    def validate(self, data):
+        print(data)
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+        username_pattern = r'^[A-Za-z][A-Za-z0-9]{3,19}$'
+        email_pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+        password_pattern = r'^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};\'":\\|,.<>\/?]).{6,}$'
+
+        if not self._check(username_pattern, username):
+            raise serializers.ValidationError({"detail": "Error username validation"})
+        
+        if not self._check(email_pattern, email):
+            raise serializers.ValidationError({"detail": "Error email validation"})
+        
+        if not self._check(password_pattern, password):
+            raise serializers.ValidationError({"detail": "Error username validation"})
+        
+        return data
+
+
+    def _check(self, pattern, value):        
+        if match(pattern, value) is not None:
+            return True
